3e62b2545d6025a9011e3636cd5155fc63fc5748
Jens Ungerer <github@jens-ungerer.de>
2021-05-17 08:59:47 +0200

    chaning test from xunit to nunit

---
+++ BitVector.Test/InstantiationTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace BitVector.Test
+{
+    public class InstantiationTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

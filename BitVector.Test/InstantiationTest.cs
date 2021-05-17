5df36734123b61932eeb80c41a529577ddfae146
Jens Ungerer <github@jens-ungerer.de>
2021-05-17 09:07:25 +0200

    first basic test

--- BitVector.Test/InstantiationTest.cs
+++ BitVector.Test/InstantiationTest.cs
@@ -4,15 +4,30 @@ namespace BitVector.Test
 {
     public class InstantiationTest
     {
+
+        private static BitVector CreateFullBitVector(BitVector bitVector, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                bitVector[i] = true;
+            }
+            return bitVector;
+        }
+
         [SetUp]
         public void Setup()
         {
         }
 
         [Test]
-        public void Test1()
+        public void CreatedInstanceEmptyCardinality()
         {
-            Assert.Pass();
+            // ARRANGE
+            var size = 64;
+            var bitVector = new BitVector(size);
+
+            // ASSERT
+            Assert.AreEqual(0, bitVector.Cardinality);
         }
     }
 }
